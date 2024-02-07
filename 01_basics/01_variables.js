69c2a6cf72d26775c7a18b92399840972bebfabe
Jayesh3375 <159343611+Jayesh3375@users.noreply.github.com>
2024-02-07 21:11:35 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,4 +5,20 @@ accountCity = "Udaipur"
 
 // accountId = 2  //not allowed
 
+accountEmail="jayesh3375@gmail.com"
+accountPassword="12345"
+accountCity = "Jaipur"
+let accountState;
+
 console.log(accountId);
+
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope.
+*/
+
+/*
+ If we declare a variable but not initialized it then it can be executed as undefined. 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
