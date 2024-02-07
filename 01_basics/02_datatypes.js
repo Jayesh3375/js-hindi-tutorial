7615331a12c321766cd9d851ea1e03784277e613
Jayesh3375 <159343611+Jayesh3375@users.noreply.github.com>
2024-02-07 21:34:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version.
+
+//alert(3 + 3) // we are using nodejs, not browser.
+
+let name = "Jayesh";   // string
+let age = 23;        //number
+let isLoggedIn =  false;  // boolean
+let state;         //undefined
+
+// Primitive Datatypes:-
+// string => ""
+// number(integer) => 2 to powerof 53 either 52 
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);  // => undefined
+console.log(typeof null);       // => object
