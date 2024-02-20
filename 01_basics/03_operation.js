d3df00e7a34a965c42ecf16dd14789dec27fc8d9
Jayesh3375 <159343611+Jayesh3375@users.noreply.github.com>
2024-02-20 20:10:21 +0000

    operations in js

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,12 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " jay";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+//  study about Prefix and Postfix
