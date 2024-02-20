b97d9902466b4c89711efc18bf4cb684e63cdfee
Jayesh3375 <159343611+Jayesh3375@users.noreply.github.com>
2024-02-20 19:47:00 +0000

    Conversion of  datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,40 @@
+ let score = 33;
+
+//  console.log(typeof score);            //output 33=> number && "33" => string
+//  console.log(typeof (score));           // output => number
+
+ let valueInNumber = Number(score);     //convert into Number
+//  console.log(typeof valueInNumber);     // Number
+//  console.log(valueInNumber);
+
+ //"33" => number
+// "33abc" => NaN
+// true => 1 && false => 0
+// null ki value => 0
+
+ let score1 = "33";
+
+//  console.log(typeof score1);            //output "33" => string
+//  console.log(typeof (score1));           // output => string
+
+let isLoggedIn = "jay";
+// console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  console.log(typeof booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+
+ //1 => true
+ //0 => false
+ // "jay" => true
+ // "" => false
+
+
+ let someNumber = 33;
+
+ let stringNumber = String(someNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
